526c6310a7a6275cd8b95c92ddb9ee905af54473
Jesse Stuart <email@jessestuart.ca>
2019-07-04 16:26:49 -0400

    dashboardv2: fix useAppRelease (stream deployments instead of releases)
    Signed-off-by: Jesse Stuart <email@jessestuart.ca>

--- dashboardv2/src/useAppRelease.ts
+++ dashboardv2/src/useAppRelease.ts
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import useClient from './useClient';
 import { setNameFilters, setPageSize, setStreamCreates, setStreamUpdates } from './client';
-import { Release } from './generated/controller_pb';
+import { Release, ExpandedDeployment, DeploymentStatus } from './generated/controller_pb';
 
 export default function useAppRelease(appName: string) {
 	const client = useClient();
@@ -10,14 +10,18 @@ export default function useAppRelease(appName: string) {
 	const [error, setError] = React.useState<Error | null>(null);
 	React.useEffect(
 		() => {
-			const cancel = client.streamReleases(
-				(releases: Release[], error: Error | null) => {
+			const cancel = client.streamDeployments(
+				(deployments: ExpandedDeployment[], error: Error | null) => {
 					setIsLoading(false);
 					if (error) {
 						setError(error);
 						return;
 					}
-					setRelease(releases[0] || new Release());
+					const deployment = deployments[0] || new ExpandedDeployment();
+					if (deployment.getStatus() !== DeploymentStatus.COMPLETE) {
+						return;
+					}
+					setRelease(deployment.getNewRelease() || new Release());
 					setError(null);
 				},
 				setNameFilters(appName),
